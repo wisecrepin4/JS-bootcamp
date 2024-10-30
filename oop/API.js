bf0e422e28e4292a2e358273edd382d5c50f3858
wisecrepin <ngomawisecrepin@gmail.com>
2024-10-29 15:38:53 -1200

    finishing the promise chaining for fetch api

--- oop/API.js
+++ oop/API.js
@@ -1,5 +1,20 @@
+const getPuzzle = (wordcount) => {
+  fetch(`https://puzzle.mead.io/puzzle?wordCount=${wordcount}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("the url is broken data can't be found");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(`the word is ${data.puzzle}`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
 const getCountry = (alphacode) => {
-  let countryreq;
   return fetch("https://restcountries.com/v2/all")
     .then((response) => {
       if (response.ok) {
@@ -18,53 +33,36 @@ const getCountry = (alphacode) => {
     .catch((error) => {
       console.log(error);
     });
-
-  const getPuzzle = (wordcount) => {
-    fetch(`https://puzzle.mead.io/puzzle?wordCount=${wordcount}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("the url is broken data can't be found");
-        }
+};
+// new Promise((resolve, reject) => {
+//   const request = new XMLHttpRequest();
+//   request.addEventListener("readystatechange", () => {
+//     if (request.status == 200 && request.readyState == 4) {
+//       let data = JSON.parse(request.responseText);
+//       resolve(data.puzzle);
+//     } else if (request.readyState == 4) {
+//       reject("error has occured");
+//     }
+//   });
+//   request.open("GET", `https://puzzle.mead.io/puzzle?wordCount=${wordcount}`);
+//   request.send();
+// });
+let getlocation = () => {
+  fetch("https://ipinfo.io/json?token=fd32c68aa2204d")
+    .then((response) => {
+      if (response.ok) {
         return response.json();
-      })
-      .then((data) => {
-        console.log(`the word is ${data.puzzle}`);
-      })
-      .catch((err) => {
-        console.log(err);
+      } else {
+        throw new Error("the link is not valid to show the Ip address");
+      }
+    })
+    .then((data) => {
+      getCountry(data.country).then((count) => {
+        console.log(`you live In ${data.city} ${count}`);
       });
-  };
-  // new Promise((resolve, reject) => {
-  //   const request = new XMLHttpRequest();
-  //   request.addEventListener("readystatechange", () => {
-  //     if (request.status == 200 && request.readyState == 4) {
-  //       let data = JSON.parse(request.responseText);
-  //       resolve(data.puzzle);
-  //     } else if (request.readyState == 4) {
-  //       reject("error has occured");
-  //     }
-  //   });
-  //   request.open("GET", `https://puzzle.mead.io/puzzle?wordCount=${wordcount}`);
-  //   request.send();
-  // });
-  let getlocation = () => {
-    fetch("https://ipinfo.io/json?token=fd32c68aa2204d")
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("the link is not valid to show the Ip address");
-        }
-      })
-      .then((data) => {
-        return getCountry(data.country);
-      })
-      .then((country) => {
-        console.log(`you live in ${data.city} ${country}`);
-      })
+    })
 
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+    .catch((err) => {
+      console.log(err);
+    });
 };
